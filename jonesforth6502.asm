ba9a1ac38e0e1c50c5a0bf1fd8803d536e3f1c17
derheap <derheap@users.noreply.github.com>
2025-08-16 01:21:24 +0200

    DOCOL

--- jonesforth6502.asm
+++ jonesforth6502.asm
@@ -26,29 +26,63 @@ LoopI   STA PSP,x
         INX
         CPX #$00
         BNE LoopI
-Halt
-        JMP Halt
         
-	MAC NEXT
+        JMP TEST0
+   
+; NEXT
+	MAC NEXTM
         ; IP -> W
         ; IP+2
         CLC
-        LDA IP+1
-        STA W+1
-        ADC #2
         LDA IP
         STA W
-        ADC #0
+        ADC #2
         STA IP
+        LDA IP+1
+        STA W+1
+        ADC #0
+        STA IP+1
         ; (W) -> X
         LDY #0
-        LDA (W),Y
-        STA X,Y
+        LDA (W),y
+        STA X,y
         INY
-        LDA (W),Y
-        STA X,Y
+        LDA (W),y
+        STA X,y
         ; JMP (X)
         JMP (X)
         ENDM
+; Helper  
+        MAC PUSHRSP
+        LDA IP
+        PHA
+        LDA IP+1
+        PHA
+        ENDM
         
-TEST0	NEXT
+        MAC POPRSP
+        PLA
+        STA IP+1
+        PLA
+        STA IP
+        ENDM
+        
+; DOCOL
+
+DOCOL	PUSHRSP
+	CLC
+	LDA W
+        ADC #2
+        STA IP
+        LDA W+1
+        STA IP+1
+NEXT    NEXTM
+        
+FAKE	DC.W FAKE+2, FAKE+4
+HALT	JMP HALT
+        
+TEST0	LDA #>FAKE
+	STA IP+1
+        LDA #<FAKE
+        STA IP
+	NEXTM
