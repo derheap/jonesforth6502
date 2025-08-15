8ef35ab68377e75d622763ac3839f2074ea3038a
derheap <derheap@users.noreply.github.com>
2025-08-15 15:57:49 +0200

    Next macro

--- jonesforth6502.asm
+++ jonesforth6502.asm
@@ -5,8 +5,50 @@
 
 	seg CODE
 	org $803	; starting address
+        
+;          Figure 5. Register Assignments
+;
+;            W     IP    PSP   RSP   UP     TOS 
+;
+;6502        Zpage Zpage X     SP    Zpage  memory  [KUN81]
 
-Start
-	lda #$32
-        sta $400
-        jmp Start
+Base	equ $50
+IP	equ Base
+W	equ Base+2
+X	equ Base+4
+PSP	equ $300
+
+Init
+	; Clear PSP
+	LDX #$00
+        TXA
+LoopI   STA PSP,x
+        INX
+        CPX #$00
+        BNE LoopI
+Halt
+        JMP Halt
+        
+	MAC NEXT
+        ; IP -> W
+        ; IP+2
+        CLC
+        LDA IP+1
+        STA W+1
+        ADC #2
+        LDA IP
+        STA W
+        ADC #0
+        STA IP
+        ; (W) -> X
+        LDY #0
+        LDA (W),Y
+        STA X,Y
+        INY
+        LDA (W),Y
+        STA X,Y
+        ; JMP (X)
+        JMP (X)
+        ENDM
+        
+TEST0	NEXT
